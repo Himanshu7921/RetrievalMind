8eb6542b47288f31a7c6eb94d51f554771b4af51
Himanshu7921 <himanshusr451tehs@gmail.com>
2025-10-18 14:55:29 +0530

    Changed Project structure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="RetrievalMind",
-    version="0.1.0",
+    version="0.1.1",
     author="Himanshu Singh",
     author_email="himanshu@example.com",
     description="A custom Retrieval-Augmented Generation (RAG) framework for AI Agent applications.",
